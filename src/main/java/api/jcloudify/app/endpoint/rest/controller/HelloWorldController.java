48ccb8e686eead73e8795742e1b42e60a878da64
YumeT023 <hei.hajatiana@gmail.com>
2024-11-08 08:29:16 +0300

    feat: add GET /hello-world

---
+++ src/main/java/api/jcloudify/app/endpoint/rest/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package api.jcloudify.app.endpoint.rest.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+  @GetMapping("/hello-world")
+  public String helloWorld() {
+    return "Hello world from JCloudify!";
+  }
+}
